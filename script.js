5a87938847bd2c82575cea0e3426135a76faceb8
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-02-10 20:27:15 -0700

    began adding variables to script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Assignment Code
+var chars = ["0123456789qwertyuiopasdfghjklzxcvbnm!@#$%^&*(){}:"];
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
